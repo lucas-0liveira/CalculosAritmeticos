d1624ece2d9c411f60c08ac26e08485d2de7972c
Lucas de Oliveira Fernandes <82547431+lucas-0liveira@users.noreply.github.com>
2022-03-28 12:51:44 -0300

    Calculo Aritméticos

---
+++ CalculoAritmeticos/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace ExercícioDAL15_03_2022
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResto = new System.Windows.Forms.Label();
+            this.txtResto = new System.Windows.Forms.TextBox();
+            this.txtDivisao = new System.Windows.Forms.TextBox();
+            this.txtMultiplicacao = new System.Windows.Forms.TextBox();
+            this.txtSubtracao = new System.Windows.Forms.TextBox();
+            this.txtSoma = new System.Windows.Forms.TextBox();
+            this.lblDivisao = new System.Windows.Forms.Label();
+            this.lblMultiplicacao = new System.Windows.Forms.Label();
+            this.lblSubtracao = new System.Windows.Forms.Label();
+            this.lblSoma = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtValor2 = new System.Windows.Forms.TextBox();
+            this.txtValor1 = new System.Windows.Forms.TextBox();
+            this.lblValor2 = new System.Windows.Forms.Label();
+            this.lblValor1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblResto
+            // 
+            this.lblResto.AutoSize = true;
+            this.lblResto.Location = new System.Drawing.Point(67, 151);
+            this.lblResto.Name = "lblResto";
+            this.lblResto.Size = new System.Drawing.Size(46, 16);
+            this.lblResto.TabIndex = 29;
+            this.lblResto.Text = "Resto:";
+            // 
+            // txtResto
+            // 
+            this.txtResto.Location = new System.Drawing.Point(135, 145);
+            this.txtResto.Name = "txtResto";
+            this.txtResto.Size = new System.Drawing.Size(100, 22);
+            this.txtResto.TabIndex = 28;
+            // 
+            // txtDivisao
+            // 
+            this.txtDivisao.Location = new System.Drawing.Point(135, 117);
+            this.txtDivisao.Name = "txtDivisao";
+            this.txtDivisao.Size = new System.Drawing.Size(100, 22);
+            this.txtDivisao.TabIndex = 27;
+            // 
+            // txtMultiplicacao
+            // 
+            this.txtMultiplicacao.Location = new System.Drawing.Point(135, 88);
+            this.txtMultiplicacao.Name = "txtMultiplicacao";
+            this.txtMultiplicacao.Size = new System.Drawing.Size(100, 22);
+            this.txtMultiplicacao.TabIndex = 26;
+            // 
+            // txtSubtracao
+            // 
+            this.txtSubtracao.Location = new System.Drawing.Point(135, 55);
+            this.txtSubtracao.Name = "txtSubtracao";
+            this.txtSubtracao.Size = new System.Drawing.Size(100, 22);
+            this.txtSubtracao.TabIndex = 25;
+            // 
+            // txtSoma
+            // 
+            this.txtSoma.Location = new System.Drawing.Point(135, 24);
+            this.txtSoma.Name = "txtSoma";
+            this.txtSoma.Size = new System.Drawing.Size(100, 22);
+            this.txtSoma.TabIndex = 24;
+            // 
+            // lblDivisao
+            // 
+            this.lblDivisao.AutoSize = true;
+            this.lblDivisao.Location = new System.Drawing.Point(67, 120);
+            this.lblDivisao.Name = "lblDivisao";
+            this.lblDivisao.Size = new System.Drawing.Size(56, 16);
+            this.lblDivisao.TabIndex = 23;
+            this.lblDivisao.Text = "Divisão:";
+            // 
+            // lblMultiplicacao
+            // 
+            this.lblMultiplicacao.AutoSize = true;
+            this.lblMultiplicacao.Location = new System.Drawing.Point(40, 91);
+            this.lblMultiplicacao.Name = "lblMultiplicacao";
+            this.lblMultiplicacao.Size = new System.Drawing.Size(89, 16);
+            this.lblMultiplicacao.TabIndex = 22;
+            this.lblMultiplicacao.Text = "Multiplicação:";
+            // 
+            // lblSubtracao
+            // 
+            this.lblSubtracao.AutoSize = true;
+            this.lblSubtracao.Location = new System.Drawing.Point(51, 58);
+            this.lblSubtracao.Name = "lblSubtracao";
+            this.lblSubtracao.Size = new System.Drawing.Size(72, 16);
+            this.lblSubtracao.TabIndex = 21;
+            this.lblSubtracao.Text = "Subtração:";
+            // 
+            // lblSoma
+            // 
+            this.lblSoma.AutoSize = true;
+            this.lblSoma.Location = new System.Drawing.Point(77, 27);
+            this.lblSoma.Name = "lblSoma";
+            this.lblSoma.Size = new System.Drawing.Size(46, 16);
+            this.lblSoma.TabIndex = 20;
+            this.lblSoma.Text = "Soma:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(123, 134);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 19;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtValor2
+            // 
+            this.txtValor2.Location = new System.Drawing.Point(109, 106);
+            this.txtValor2.Name = "txtValor2";
+            this.txtValor2.Size = new System.Drawing.Size(100, 22);
+            this.txtValor2.TabIndex = 18;
+            // 
+            // txtValor1
+            // 
+            this.txtValor1.Location = new System.Drawing.Point(109, 70);
+            this.txtValor1.Name = "txtValor1";
+            this.txtValor1.Size = new System.Drawing.Size(100, 22);
+            this.txtValor1.TabIndex = 17;
+            // 
+            // lblValor2
+            // 
+            this.lblValor2.AutoSize = true;
+            this.lblValor2.Location = new System.Drawing.Point(41, 109);
+            this.lblValor2.Name = "lblValor2";
+            this.lblValor2.Size = new System.Drawing.Size(52, 16);
+            this.lblValor2.TabIndex = 16;
+            this.lblValor2.Text = "Valor 2:";
+            // 
+            // lblValor1
+            // 
+            this.lblValor1.AutoSize = true;
+            this.lblValor1.Location = new System.Drawing.Point(41, 73);
+            this.lblValor1.Name = "lblValor1";
+            this.lblValor1.Size = new System.Drawing.Size(52, 16);
+            this.lblValor1.TabIndex = 15;
+            this.lblValor1.Text = "Valor 1:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblResto);
+            this.groupBox1.Controls.Add(this.lblSoma);
+            this.groupBox1.Controls.Add(this.txtResto);
+            this.groupBox1.Controls.Add(this.lblSubtracao);
+            this.groupBox1.Controls.Add(this.txtDivisao);
+            this.groupBox1.Controls.Add(this.lblMultiplicacao);
+            this.groupBox1.Controls.Add(this.txtMultiplicacao);
+            this.groupBox1.Controls.Add(this.lblDivisao);
+            this.groupBox1.Controls.Add(this.txtSubtracao);
+            this.groupBox1.Controls.Add(this.txtSoma);
+            this.groupBox1.Location = new System.Drawing.Point(292, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(250, 186);
+            this.groupBox1.TabIndex = 30;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Aritméticas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 221);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtValor2);
+            this.Controls.Add(this.txtValor1);
+            this.Controls.Add(this.lblValor2);
+            this.Controls.Add(this.lblValor1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResto;
+        private System.Windows.Forms.TextBox txtResto;
+        private System.Windows.Forms.TextBox txtDivisao;
+        private System.Windows.Forms.TextBox txtMultiplicacao;
+        private System.Windows.Forms.TextBox txtSubtracao;
+        private System.Windows.Forms.TextBox txtSoma;
+        private System.Windows.Forms.Label lblDivisao;
+        private System.Windows.Forms.Label lblMultiplicacao;
+        private System.Windows.Forms.Label lblSubtracao;
+        private System.Windows.Forms.Label lblSoma;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtValor2;
+        private System.Windows.Forms.TextBox txtValor1;
+        private System.Windows.Forms.Label lblValor2;
+        private System.Windows.Forms.Label lblValor1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ CalculoAritmeticos/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExercícioDAL15_03_2022
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int valor1, valor2, soma, subtracao, multiplicacao, divisao, resto;
+
+            valor1 = Convert.ToInt32(txtValor1.Text);
+            valor2 = Convert.ToInt32(txtValor2.Text);
+
+            soma = valor1 + valor2;
+            subtracao = valor1 - valor2;
+            multiplicacao = valor1 * valor2;
+            divisao = valor1 / valor2;
+            resto = valor1 % valor2;
+
+            txtSoma.Text = soma.ToString();
+            txtSubtracao.Text = subtracao.ToString();
+            txtMultiplicacao.Text = multiplicacao.ToString();
+            txtDivisao.Text = divisao.ToString();
+            txtResto.Text = resto.ToString() ;
+
+        }
+    }
+}
